0d04ac5ad1ae699a6e360187222880bd80548d6b
Lacey Williams Henschel <laceynwilliams@gmail.com>
2017-11-09 14:11:41 -0800

    Logical operator solutions; syntax correction

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -2,7 +2,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.logicalOperatorsAnswers = {
   or: function(a, b) {
-    return a  || b;
+    return a || b;
   },
 
   and: function(a, b) {
